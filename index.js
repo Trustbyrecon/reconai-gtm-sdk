e2bc95a51f046cd3dbe8ffb06950aeec3fe12d28
reconai-dev <clark.dwayne@gmail.com>
2025-07-05 15:24:58 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end('<h1>ReconAI GTM SDK (Node Server)</h1>');
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}/`);
+});
