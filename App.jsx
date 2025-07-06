afb8505aa2461948d9ab248e7b8ff731aa199c99
reconai-dev <clark.dwayne@gmail.com>
2025-07-06 16:20:55 -0400

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,17 @@
+
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>🚀 Welcome to ReconAI GTM SDK</h1>
+        <p>This is your starter UI template.</p>
+        <button onClick={() => alert('Trust Activated!')}>Run Trust Protocol</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
